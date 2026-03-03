8a74b7b107564511cb52cd6a97923e94f7a9d44e
Luffy-456 <anshuman567.in@gmail.com>
2026-03-03 23:48:15 +0530

    [1] [Time Beats: 72.97%] [Memory Beats: 18.27%] - LeetPush

--- easy/1. Two Sum/Two-Sum.cpp
+++ easy/1. Two Sum/Two-Sum.cpp
@@ -1,20 +1,15 @@
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        int size=nums.size();
-5        vector<int>ans;
-6        for(int i=0;i<size;i++){
-7            int t=target-nums[i];
-8            for(int j=i+1;j<size;j++){
-9                if(i!=j){
-10                    if(t==nums[j]){
-11                        ans.push_back(i);
-12                        ans.push_back(j);
-13                        break;
-14                    }
-15                }
-16            }
-17        }
-18        return ans;
-19    }
-20};
+4       map<int,int>mp;
+5       int n=nums.size();
+6       for(int i=0;i<n;i++){
+7        int req=target-nums[i];
+8        if(mp.find(req)!=mp.end()){
+9            return {mp[req],i};
+10        }
+11        mp[nums[i]]=i;
+12       }
+13       return {-1,-1};
+14    }
+15};
