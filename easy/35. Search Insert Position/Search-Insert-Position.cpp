0e03d5950443aa91cfdc80d6826b245be3b16c96
Luffy-456 <anshuman567.in@gmail.com>
2026-03-17 21:56:41 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 42.73%] - LeetPush

--- easy/35. Search Insert Position/Search-Insert-Position.cpp
+++ easy/35. Search Insert Position/Search-Insert-Position.cpp
@@ -1,25 +1,33 @@
-1int bs(vector<int>&nums,int target){
-2    int l=0,h=nums.size()-1;
-3    while(l<=h){
-4        int m=(l+h)/2;
-5        if(nums[m]==target){
-6            return m;
-7        }
-8        else if(target>nums[m]){
-9            l=m+1;
-10        }
-11        else{
-12            h=m-1;
-13        }
-14    }
-15    return l;
-16}
-17
-18class Solution {
+1class Solution {
+2private:
+3    int bs(vector<int>&nums,int target){
+4        int l=0,h=nums.size()-1;
+5        while(l<=h){
+6            int m=l+((h-l)/2);
+7            if(nums[m]==target){
+8                return m;
+9            }
+10            else if(target>nums[m]){
+11                l=m+1;
+12            }
+13            else{
+14                h=m-1;
+15            }
+16        }
+17        return l;
+18    }
 19public:
 20    int searchInsert(vector<int>& nums, int target) {
-21        int size=nums.size();
-22        int ans=bs(nums,target);
-23return ans;
-24    }
-25};
+21        int ans=bs(nums,target);
+22return ans;
+23    }
+24};
+25
+26//made some optimizations
+27
+28//here l will return the insertion index
+29// here h will return the floor value index & ceil value index 
+30// here h will also return the from where the higher elements started from the target ex:
+31//nums = [1,3,5,6], target = 4
+32//l = 2  (insert at index 2)
+33//h = 1  (element 3)
