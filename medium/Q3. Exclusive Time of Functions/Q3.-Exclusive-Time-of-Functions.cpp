375a4cfabcf595ea52d4c1239d9a9d5fbb01e320
Luffy-456 <anshuman567.in@gmail.com>
2026-03-28 22:41:52 +0530

    [Q3] [Time Beats: 73.51%] [Memory Beats: 80.88%] - LeetPush

---
+++ medium/Q3. Exclusive Time of Functions/Q3.-Exclusive-Time-of-Functions.cpp
@@ -0,0 +1,42 @@
+1class Solution {
+2public:
+3    vector<int> exclusiveTime(int n, vector<string>& logs) {
+4        int size=logs.size();
+5        vector<int>ans(n,0);
+6        stack<int>st;
+7        int prv_time=0;
+8        for(int i=0;i<size;i++){
+9            string tmp=logs[i];
+10// reading log
+11            stringstream ss(tmp);
+12            string id_str, type, time_str;
+13
+14            getline(ss,id_str,':');
+15            getline(ss,type,':');
+16            getline(ss,time_str,':');
+17            int id=stoi(id_str);
+18            int s_time=stoi(time_str);
+19
+20        if(type=="start"){
+21
+22            if(!st.empty()){
+23                ans[st.top()]+=s_time-prv_time;
+24            }
+25// always push even if stack is empty or not
+26                st.push(id);
+27                prv_time=s_time;
+28        }
+29        else{
+30// +1 cause the end time is inclsive
+31                ans[st.top()]+=s_time-prv_time+1;
+32                st.pop();
+33                prv_time=s_time+1;
+34            }
+35
+36        }
+37
+38
+39    return ans;
+40        
+41    }
+42};
