ea3abca4af04eb1d04d3a5ff76dd8f2e4eec29aa
Luffy-456 <anshuman567.in@gmail.com>
2026-04-25 18:40:44 +0530

    [121] [Time Beats: 100.00%] [Memory Beats: 24.29%] - LeetPush

--- easy/121. Best Time to Buy and Sell Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
+++ easy/121. Best Time to Buy and Sell Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -1,14 +1,28 @@
-1class Solution {
-2public:
-3    int maxProfit(vector<int>& prices) {
-4        int mini=prices[0];
-5        int profit=0;
-6        int n=prices.size();
-7        for(int i=1;i<n;i++){
-8            int cost=prices[i]-mini;
-9            profit=max(cost,profit);
-10            mini=min(mini,prices[i]);
-11        }
-12        return profit;
-13    }
-14};
+1//optimal
+2class Solution {
+3public:
+4    int maxProfit(vector<int>& a) {
+5        int ans=0,mini=INT_MAX;
+6        for(int i=0;i<a.size();i++){
+7            mini=min(mini,a[i]);
+8                int profit=a[i]-mini;
+9                ans=max(ans,profit);
+10        }
+11        return ans;
+12    }
+13};
+14//brute force
+15// class Solution {
+16// public:
+17//     int maxProfit(vector<int>& a) {
+18//         int ans=0;
+19//         for(int i=0;i<a.size();i++){
+20//             for(int j=i+1;j<a.size();j++){
+21//                 int profit=a[j]-a[i];
+22//                 ans=max(ans,profit);
+23//             }
+24//         }
+25//         return ans;
+26//     }
+27// };
+28
